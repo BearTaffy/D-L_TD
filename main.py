2e7c19368662075d106e4002f5f67e3ce39c2a04
BearTaffy <davisulmanis5@gmail.com>
2024-10-10 09:44:52 +0300

    gargoyle and golem model + added cam change func

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import vizinfo
 import random
 import math
 
+# TODO: refactor code
 
 viz.mouse.setVisible(False)
 
@@ -16,9 +17,12 @@ mapp.setPosition(0, -1, 0)
 
 day = viz.add("sky_day.osgb")
 day.renderToBackground()
+camMode = "robot"
 
 
 robot = viz.add("models/robot.obj")
+# golem = viz.add("models/big_golem.obj")
+
 
 robot.setPosition([-1, -1, 2])
 robot.setScale([0.1, 0.1, 0.1])
@@ -31,10 +35,6 @@ viz.go(viz.FULLSCREEN)
 viz.clearcolor(viz.SKYBLUE)
 
 # Cam/Movement
-viewLink = viz.link(robot, viz.MainView)
-viewLink.preEuler([0, 45, 0])
-viewLink.preTrans([0, 0, -3])
-viewLink.preEuler([0, -20, 0])
 
 navigator = vizcam.addWalkNavigate(moveScale=2.0)
 viz.cam.setHandler(navigator)
@@ -42,6 +42,39 @@ viz.MainView.collision(viz.OFF)
 
 viz.MainView.setPosition(0, 0, 0)
 viz.MainView.setEuler(0, 0, 0)
+
+
+downCam = vizshape.addSphere(radius=0.1)
+downCam.setPosition(0, 11, 1)
+
+viewLink = viz.link(robot, viz.MainView)
+viewLink.preEuler([0, 45, 0])
+viewLink.preTrans([0, 0, -3])
+viewLink.preEuler([0, -20, 0])
+# viewLink.preEuler([0, 90, 0])
+
+
+def onKeyDown(key):
+    if key == "q":
+        changeCamera()
+        # print("Camera changed")
+        # print(viz.MainView)
+        # print(viewLink)
+
+
+def changeCamera():
+    global camMode
+    if camMode == "robot":
+        viewLink = viz.link(downCam, viz.MainView)
+        viewLink.preEuler([0, 90, 0])
+        camMode = "downCam"
+    else:
+        viewLink = viz.link(robot, viz.MainView)
+        viewLink.preEuler([0, 45, 0])
+        viewLink.preTrans([0, 0, -3])
+        viewLink.preEuler([0, -20, 0])
+        camMode = "robot"
+
 
 robotLink = viz.link(navigator, robot)
 robotLink.postTrans([0, -1, 1])
@@ -56,6 +89,7 @@ dir_light = viz.addDirectionalLight(color=viz.WHITE, euler=(45, 0, 45))
 dir_light.direction(0, -1, 0)
 dir_light.intensity(0.5)
 
+viz.callback(viz.KEYDOWN_EVENT, onKeyDown)
 
 if __name__ == "__main__":
     viz.go()
