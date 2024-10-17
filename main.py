b1bfd4b5301a239dab8ef78c7f661c1c32fa442f
L-Augsstkalnietis <laurisaugstkalnietis@gmail.com>
2024-10-17 10:52:38 +0300

    New rock
    Rock and Stone

--- main.py
+++ main.py
@@ -36,12 +36,17 @@ tree=viz.add("models/tree.obj")
 tree.setPosition(-8.4,-1,-7.7)
 tree.setScale([0.5, 0.5, 0.5])
 
+stone=viz.add("models/rock_formation.obj")
+stone.setPosition(-8.87,-0.8, 7.08)
+stone.setScale([0.5, 0.5, 0.5])
+
 
 # Variables
 camMode = "robot"
 wood_count = 0
 stone_count = 0
 collecting_wood = False
+collecting_stone = False
 
 # Create text for resource counters
 wood_text = viz.addText(f"Wood: {wood_count}", pos=[0.1, 0.9, 0], parent=viz.SCREEN)
@@ -106,11 +111,24 @@ robot.setScale([0.1, 0.1, 0.1])
 robot.setEuler([0, 0, 0])
 
 def add_wood():
-    global wood_count, collecting_wood
+    global wood_count, collecting_wood, stone_count, collecting_stone
     if collecting_wood:  
         wood_count += 1
         update_resources()
+    if collecting_stone:  
+        stone_count += 1
+        update_resources()
     
+def start_collecting_wood():
+    # This will call add_wood every 2 seconds as long as collecting_wood is True
+    def wood_timer():
+        if collecting_wood:
+            add_wood()
+    def stone_timer():
+        if collecting_stone:
+            add_wood()
+    vizact.ontimer(2, wood_timer)
+    vizact.ontimer(2, stone_timer)
 
 
 # Proximity manager setup
@@ -118,24 +136,35 @@ manager = vizproximity.Manager()
 manager.setDebug(True)
 manager.addTarget(vizproximity.Target(robot))
 wood_timer = None
+stone_timer= None
 # Enter sensor function
 def onEnterSensor(e):
-    global collecting_wood
-    if e.sensor.name == 'Circle':
+   global collecting_wood, collecting_stone
+   if e.sensor.name == 'Circle':
         viz.logNotice('Entered wood collection area')
-        collecting_wood= True
-        wood_timer = vizact.ontimer2(2, 0, add_wood) 
+        collecting_wood = True
+        start_collecting_wood()  # Start the repeating timer when entering the area
+   if e.sensor.name == 'Circle2':
+        viz.logNotice('Entered stone collection area')
+        collecting_stone = True
+        start_collecting_wood()  # Start the repeating timer when entering the area
         
 
 # Exit sensor function
 def onExitSensor(e):
-    global collecting_wood, wood_timer
+    global collecting_wood, wood_timer, collecting_stone, stone_timer
     if e.sensor.name == 'Circle':
         viz.logNotice('Left wood collection area')
         collecting_wood = False
         if wood_timer:
             wood_timer.remove()
             wood_timer = None
+    if e.sensor.name == 'Circle2':
+        viz.logNotice('Left stone collection area')
+        collecting_stone = False
+        if stone_timer:
+            stone_timer.remove()
+            stone_timer = None
 
 # Add proximity event callbacks
 manager.onEnter(None, onEnterSensor)
@@ -150,6 +179,9 @@ def AddSensor(shape, name):
 # Add circular sensor around the tree
 shape = vizproximity.CircleArea(3, center=[-8.21, -7.7])
 AddSensor(shape, 'Circle')
+
+shape = vizproximity.CircleArea(3, center=[-8.87, 7.08])
+AddSensor(shape, 'Circle2')
 
 # Screen setup
 viz.setMultiSample(4)
