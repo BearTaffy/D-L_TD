b18649cc1e96d85fca1b15d3bf94aa7553f99ef4
L-Augsstkalnietis <laurisaugstkalnietis@gmail.com>
2024-10-29 13:03:45 +0200

    on the title screeen

--- infoScreen.py
+++ infoScreen.py
@@ -16,6 +16,7 @@ def TitleScreenTask():
                 display: flex;
                 align-items: center;
                 justify-content: center;
+                flex-direction: column;
                 height: 100vh;
                 overflow: hidden;
             }
@@ -24,17 +25,33 @@ def TitleScreenTask():
                 font-size: 50px;
                 color: #ffffff;
                 text-align: center;
-                background-color: rgba(0, 0, 0, 0.5); /* Optional background for text readability */
                 padding: 1vh 2vw;
                 border-radius: 10px;
                 margin-top: 15%;
                 margin-left: 7%;
+                text-shadow: 2px 2px 4px #000000;
             }
+
+            button {
+                font-size: 30px;
+                padding: 10px 20px;
+                color: #ffffff;
+                background-color: #9d99a3;
+                border: none;
+                border-radius: 20px;
+                cursor: pointer;
+                text-shadow: 1px 1px 3px #000000;
+                margin-left:42%;
+                margin-top: 3%;
+            }
+
+            
         </style>
     </head>
     <body>
         <h1>Tower Defense</h1>
-        <button>test</button>
+        <button onclick="window.location.href='javascript:parent.viz.sendEvent(\\'startGame\\')'">Start Game</button>
+        <button onclick="window.location.href='javascript:parent.viz.sendEvent(\\'howToPlay\\')'">How to play</button>
     </body>
     </html>
     """
@@ -42,11 +59,77 @@ def TitleScreenTask():
     # Display the title screen
     vizhtml.displayCode(html)
 
-    # Wait for "Start Game" button to be pressed
-    yield viztask.waitEvent('startGame')
+    # Wait for either "Start Game" or "How to play" event
+    while True:
+        event = yield viztask.waitAny([viztask.waitEvent('startGame'), viztask.waitEvent('howToPlay')])
+        
+        if event.condition == 'startGame':
+            viz.logNotice('Game is starting...')
+            break
+        elif event.condition == 'howToPlay':
+            yield howToPlayTask()  # Call howToPlayTask when 'How to play' is clicked
 
     # Proceed to main game
     viz.logNotice('Game is starting...')
+
+
+def howToPlayTask():
+    html = """
+    <html>
+    <head>
+        <style>
+            body {
+                background: url('img/title.png') no-repeat center center;
+                background-size: cover;
+                margin: 0;
+                padding: 0;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                height: 100vh;
+                overflow: hidden;
+            }
+            
+            h1 {
+                font-size: 50px;
+                color: #ffffff;
+                text-align: center;
+                padding: 1vh 2vw;
+                border-radius: 10px;
+                margin-top: 15%;
+                margin-left: 7%;
+                text-shadow: 2px 2px 4px #000000;
+            }
+
+            button {
+                font-size: 30px;
+                padding: 10px 20px;
+                color: #ffffff;
+                background-color: #9d99a3;
+                border: none;
+                border-radius: 20px;
+                cursor: pointer;
+                text-shadow: 1px 1px 3px #000000;
+                margin-left:42%;
+                margin-top: 3%;
+            }
+
+            
+        </style>
+    </head>
+    <body>
+        <h1>How to play</h1>
+    </body>
+    </html>
+    """
+
+    # Display the title screen
+    vizhtml.displayCode(html)
+
+    # Proceed to main game
+    viz.logNotice('Game is starting...')
+
 
 # Schedule the task to run
 viztask.schedule(TitleScreenTask())
